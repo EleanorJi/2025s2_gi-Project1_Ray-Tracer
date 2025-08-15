ef968b962e6c9e0953db9c061979a1283d5aeeaa
EleanorJi <hanyuj2@student.unimelb.edu.au>
2025-08-15 21:36:40 +1000

    Stage 1.4 triangle

--- src/scene/primitives/Triangle.cs
+++ src/scene/primitives/Triangle.cs
@@ -32,8 +32,51 @@ namespace RayTracer
         /// <returns>Hit data (or null if no intersection)</returns>
         public RayHit Intersect(Ray ray)
         {
-            // Write your code here...
-            return null;
+            Vector3 edge1 = this.v1 - this.v0;
+            Vector3 edge2 = this.v2 - this.v0;
+            Vector3 normal = edge1.Cross(edge2);
+            double areaABC = normal.Length() / 2.0;
+
+            // Check if the light is parallel to the plane of the triangle
+            double nr = normal.Dot(ray.Direction);
+
+            // parallel to the plane
+            if (nr == 0)
+            {
+                return null;
+            }
+
+            // solve the t
+            Vector3 ao = ray.Origin - this.v0;
+            double t = -normal.Dot(ao) / nr;
+
+            // pposite side of origin
+            if (t < 0)
+            {
+                return null;
+            }
+
+            // Intersection point
+            Vector3 hitPoint = ray.Origin + t * ray.Direction;
+
+            // Check if P lies inside the Triangle
+            Vector3 bp = hitPoint - this.v1;
+            Vector3 cp = hitPoint - this.v2;
+            double areaBCP = (bp.Cross(cp)).Length() / 2.0;
+            double areaCAP = (edge2.Cross(-ao - t * ray.Direction)).Length() / 2.0;
+            double areaABP = ((-ao - t * ray.Direction).Cross(edge1)).Length() / 2.0;
+            double u = areaBCP / areaABC;
+            double v = areaCAP / areaABC;
+            double w = areaABP / areaABC;
+
+            // P outside the triangle
+            if (u < -1e-6 || v < -1e-6 || w < -1e-6 || Math.Abs(u + v + w - 1) > 1e-6)
+            {
+                return null;
+            }
+
+            // P inside the triangle, return the hit data
+            return new RayHit(hitPoint, normal.Normalized(), ray.Direction, this.Material);
         }
 
         /// <summary>
